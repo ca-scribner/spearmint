9bc122cc6390694c6da3d9420bd7851a62740f5d
Andrew Scribner <ca.scribner+1@gmail.com>
2020-04-13 10:12:23 -0400

    Add services/transaction.py to translate dataframes into Transaction entries
    Refactor source_file nomenclature in transaction_extractor.py for consistency with Transaction/services

--- spearmint/etl/transaction_extractor.py
+++ spearmint/etl/transaction_extractor.py
@@ -8,7 +8,7 @@ COLUMN_NAME_MAP = {
     "datetime": "Datetime",
     "description": "Description",
     "account_name": "Account Name",
-    "filename": "Source File",
+    "source_file": "Source File",
 }
 
 
@@ -16,7 +16,7 @@ class TransactionExtractor():
 
     def __init__(self):
         self.df = None
-        self.filename = None
+        self.source_file = None
 
         # Column names used for output parsed dataframe (self.df)
         self.name_map = COLUMN_NAME_MAP
@@ -28,9 +28,9 @@ class TransactionExtractor():
     def get_dataframe(self, deep=True):
         return self.df.copy(deep=deep)
 
-    def populate_from_csv(self, filename):
-        self.filename = filename
-        self.df_raw = pd.read_csv(filename, parse_dates=self.parse_dates_from)
+    def populate_from_csv(self, source_file):
+        self.source_file = source_file
+        self.df_raw = pd.read_csv(source_file, parse_dates=self.parse_dates_from)
         self._parse_raw_df()
 
     def _parse_raw_df(self):
@@ -39,15 +39,15 @@ class TransactionExtractor():
             self.name_map["description"]: self._get_raw_description(),
             self.name_map["datetime"]: self._get_raw_datetime(),
             self.name_map["account_name"]: self._get_raw_account_name(),
-            self.name_map["filename"]: self._get_raw_filename(),
+            self.name_map["source_file"]: self._get_raw_source_file(),
         }
 
         self.df = pd.DataFrame(data)
 
     @classmethod
-    def read_csv(cls, filename):
+    def read_csv(cls, source_file):
         te = cls()
-        te.populate_from_csv(filename)
+        te.populate_from_csv(source_file)
         return te
 
     # Internal methods for getting data from a raw file.  Meant to be overridden by subclasses to implement custom
@@ -64,5 +64,5 @@ class TransactionExtractor():
     def _get_raw_account_name(self):
         return self.df_raw[self.name_map["account_name"]]
 
-    def _get_raw_filename(self):
-        return self.filename
+    def _get_raw_source_file(self):
+        return self.source_file
---
+++ spearmint/services/transaction.py
@@ -0,0 +1,17 @@
+from spearmint.data.transaction import Transaction
+from spearmint.etl.transaction_extractor import COLUMN_NAME_MAP
+
+
+def dataframe_to_transactions(df, column_name_map=COLUMN_NAME_MAP):
+    return [_row_dict_to_transaction(row_dict, column_name_map) for row_dict in df.to_dict("records")]
+
+
+def _row_dict_to_transaction(row_dict, column_name_map):
+    transaction = Transaction(
+        datetime=row_dict[column_name_map["datetime"]],
+        description=row_dict[column_name_map["description"]],
+        amount=row_dict[column_name_map["amount"]],
+        account_name=row_dict[column_name_map["account_name"]],
+        source_file=row_dict[column_name_map["source_file"]],
+    )
+    return transaction
