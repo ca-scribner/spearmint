d7cd7d6b4844facb9219c31ad62626c9d8b27e2b
Andrew Scribner <ca.scribner+1@gmail.com>
2020-06-13 14:42:01 -0400

    Remove outdated support script

--- tests/manual/create_classify_test_db.sh
+++
@@ -1,9 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:`pwd`/../..
-
-db_file="test.sqlite"
-
-rm $db_file
-python -m spearmint.services.transaction add $db_file ../../secrets/pc_mc_small_sample.csv pc_mc --account_name myPcAccount
-python -m spearmint.services.transaction add $db_file ../../secrets/mint_small_sample.csv mint
-
-python -m spearmint.services.classification $db_file ../../secrets/labeledTransactions.csv
